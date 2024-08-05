b33c922a43a4122c6df565589e6ceae7a43005ec
odiak <158767461+odiak0@users.noreply.github.com>
2024-08-06 01:30:21 +0200

    misc

--- install.sh
+++ install.sh
@@ -6,14 +6,39 @@ RED="\e[31m"
 YELLOW="\e[33m"
 ENDCOLOR="\e[0m"
 
+# Function to display colored messages
+print_message() {
+    local message="$1"
+    local color="$2"
+    echo -e "${color}${message}${ENDCOLOR}"
+}
+
 # Check if pacman exists
 if ! command -v pacman &> /dev/null; then
     print_message "Error: pacman not found. This script is intended for Arch-based systems." "$RED"
     exit 1
 fi
 
+# Function to check and install Git
+check_and_install_git() {
+    if ! command -v git &> /dev/null; then
+        print_message "Git is not installed. Installing Git..." "$YELLOW"
+        sudo pacman -S --noconfirm git
+        if command -v git &> /dev/null; then
+            print_message "Git has been successfully installed." "$GREEN"
+        else
+            print_message "Failed to install Git. Please install it manually and run this script again." "$RED"
+            exit 1
+        fi
+    else
+        print_message "Git is already installed." "$GREEN"
+    fi
+}
+
 # Function to setup linuxtoolbox
 setup_linuxtoolbox() {
+    check_and_install_git
+
     LINUXTOOLBOXDIR="$HOME/linuxtoolbox"
 
     if [ ! -d "$LINUXTOOLBOXDIR" ]; then
@@ -33,13 +58,6 @@ setup_linuxtoolbox() {
     fi
 
     cd "$LINUXTOOLBOXDIR/hyprland-config" || exit
-}
-
-# Function to display colored messages
-print_message() {
-    local message="$1"
-    local color="$2"
-    echo -e "${color}${message}${ENDCOLOR}"
 }
 
 # Function to set up AUR helper
@@ -78,7 +96,7 @@ install_packages() {
         pavucontrol wl-clipboard swaybg ffmpegthumbnailer nwg-look nordic-theme
         papirus-icon-theme dunst otf-sora ttf-nerd-fonts-symbols-common otf-firamono-nerd
         inter-font ttf-fantasque-nerd noto-fonts noto-fonts-emoji ttf-jetbrains-mono-nerd
-        ttf-icomoon-feather ttf-iosevka-nerd adobe-source-code-pro-fonts brightnessctl
+        ttf-icomoon-feather ttf-iosevka-nerd adobe-source-code-pro-fonts python-requests brightnessctl
         hyprpicker
     )
 
@@ -91,6 +109,9 @@ install_packages() {
 
 # Function to move configs
 move_configs() {
+    print_message "WARNING: This will overwrite existing configuration files. Make sure you have backups if needed." "$YELLOW"
+    sleep 5  # Pause for 5 seconds to allow the user to read the warning
+
     print_message "Moving configs..." "$YELLOW"
 
     # Ask if user is using Hyprland on a laptop
@@ -171,16 +192,30 @@ move_wallpapers() {
 
 # Function to install additional fonts
 install_additional_fonts() {
-    print_message "Installing Cascadia Code font..." "$YELLOW"
+    print_message "Installing additional fonts..." "$YELLOW"
     
-    # Ensure AUR helper is set up
-    if [ -z "$helper" ]; then
-        setup_aur_helper
+    local font_dir="$HOME/Downloads/nerdfonts"
+    local font_zip="CascadiaCode.zip"
+    local font_url="https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.1/$font_zip"
+    
+    mkdir -p "$font_dir"
+    cd "$HOME/Downloads" || exit
+    
+    if wget "$font_url"; then
+        if unzip "$font_zip" -d "$font_dir"; then
+            rm -f "$font_zip"
+            if sudo mv "$font_dir" /usr/share/fonts/; then
+                fc-cache -fv
+                print_message "Cascadia Code Nerd Font installed successfully." "$GREEN"
+            else
+                print_message "Failed to move fonts to system directory." "$RED"
+            fi
+        else
+            print_message "Failed to unzip font file." "$RED"
+        fi
+    else
+        print_message "Failed to download font file." "$RED"
     fi
-
-    "$helper" -S --noconfirm --needed ttf-cascadia-code
-    
-    print_message "Cascadia Code font installed successfully" "$GREEN"
 }
 
 # Function to install NVIDIA drivers
@@ -223,7 +258,7 @@ main() {
         print_message "No wallpapers moved." "$RED"
     fi
 
-    read -rp "Would you like to install additional fonts (Cascadia Code)? (y/n) " fonts
+    read -rp "Would you like to install fonts for waybar (strongly recommended)? (y/n) " fonts
     if [[ $fonts =~ ^[Yy]$ ]]; then
         install_additional_fonts
     else
